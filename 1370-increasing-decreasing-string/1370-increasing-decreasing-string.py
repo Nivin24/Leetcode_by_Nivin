fd474f592c8af35dd74ce57652c755c5348aa03f
Nivin Benny <134007084+Nivin24@users.noreply.github.com>
2025-10-17 10:29:03 +0530

    Time: 44 ms (51.02%), Space: 12.7 MB (35.71%) - LeetHub

---
+++ 1370-increasing-decreasing-string/1370-increasing-decreasing-string.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def sortString(self, s):
+        from collections import Counter
+
+        count = Counter(s)
+        result = []
+        
+        while len(result) < len(s):
+
+            for ch in sorted(count.keys()):
+                if count[ch] > 0:
+                    result.append(ch)
+                    count[ch] -= 1
+
+            for ch in sorted(count.keys(), reverse= True):
+                if count[ch] > 0:
+                    result.append(ch)
+                    count[ch] -= 1
+
+        return ''.join(result)
