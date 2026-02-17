161c153ead93a0a1819e63bc867395644e1c3b4b
Nivin Benny <134007084+Nivin24@users.noreply.github.com>
2026-02-17 16:58:08 +0530

    Time: 7 ms (41.06%), Space: 19.4 MB (42.29%) - LeetHub

---
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minElement(self, nums: List[int]) -> int:
+        a = []
+        for n in nums:
+            summ = 0
+            sn = str(n)
+            summ = sum([int(n) for n in sn])
+            a.append(summ)
+        return min(a)
+
+
